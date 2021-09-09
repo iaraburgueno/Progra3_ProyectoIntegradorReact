5281d6ea32c71fb7f0fc28c400d7de9ca8243db1
Iara <iburgueno@udesa.edu.ar>
2021-09-09 17:40:34 -0300

    acomodamos componentes

--- src/Componentes/Nav/Nav.js
+++ src/Componentes/Nav/Nav.js
@@ -2,6 +2,30 @@ import React, {Component} from 'react';
 import './nav.css';
 
 class Nav extends Component {
+     constructor(){
+         super(); // es un metodo, permite que se hereden todos los metodos que voy a traer de component, metodo que pertenece a react
+         this.state = {
+
+         }
+     }
+     render(){
+         return(
+             <React.Fragment>
+                 <header className='header'>
+                    <h1>Título/ Nombre de la app</h1>
+                        <section className='buscador'>
+                            <p>Ordenar ASC/ DESC</p>
+                            <i className="fas fa-th"></i>
+                            <i className="fas fa-align-justify"></i>
+                            <form action="">
+                                <input type="text" name="search" id="" placeholder="Search"></input>
+                                <button type="submit"><i class="fas fa-search"></i></button>
+                            </form>
+                        </section>
+                </header>
+             </React.Fragment>
+         )
+     }
    
 }
 
